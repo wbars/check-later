be8278b8856b4f56d30fbe0d5437949e8145e7d5
Kirill Smelov <kirill.smelov@jetbrains.com>
2025-05-15 23:43:44 +0200

    Revert "Refactor webhook setup to automatically use ngrok URL and remove WEBHOOK_URL from configuration; add NgrokService for tunnel management"
    This reverts commit 19ad2eb219b331d98619a723c4d31cfe760bf1a1.

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // Required environment variables
-$dotenv->required(['BOT_API_TOKEN', 'BOT_USERNAME']);
+$dotenv->required(['BOT_API_TOKEN', 'BOT_USERNAME', 'WEBHOOK_URL']);
 $dotenv->required(['DB_DRIVER', 'DB_SQLITE_PATH']);
 
 // Legacy MySQL variables are only required if using MySQL
@@ -25,3 +25,4 @@ date_default_timezone_set('UTC');
 // Define constants
 define('BOT_API_TOKEN', $_ENV['BOT_API_TOKEN']);
 define('BOT_USERNAME', $_ENV['BOT_USERNAME']);
+define('WEBHOOK_URL', $_ENV['WEBHOOK_URL']);
